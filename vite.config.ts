b49ef6b08a20eb562897190255af30578a228c23
Vardhaman Bhandari <97441447+Vardhaman619@users.noreply.github.com>
2026-01-18 07:27:09 +0000

    removed node modules and added vercel vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import vercel from 'vite-plugin-vercel';
 
   export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),vercel()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
